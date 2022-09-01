a92761370c236ca708f76598116bf26355197727
tarekkhalifa1 <user.tarek@outlook.com>
2022-09-01 06:41:13 +0200

    modified on login input validation

--- public/js/login.js
+++ public/js/login.js
@@ -9,13 +9,14 @@ function validate(){
     let passwordInput = document.getElementById("password").value;
 
     if(login.username == userNameInput && login.password == passwordInput){
-        successLogin();
+        successLogin(); // if validation success
     }else {
-        failedLogin();
+        failedLogin(); // if validation failed
     }
 
     return document.body.appendChild(newDiv);
-}
+
+} // end of validate function
 
 
 
@@ -23,11 +24,24 @@ function successLogin(){
     newDiv.classList.remove('failed');
     newDiv.classList.add('success');
     return newDiv.innerHTML = 'Logged in Successfully, Welcome!';
-}
+
+} // end of function success message 
 
 
 function failedLogin(){
     newDiv.classList.remove('success');
     newDiv.classList.add('failed');
     return  newDiv.innerHTML = 'Wrong username or password';
-}
+
+} // end of function failed message 
+
+
+function inpFocus (elm){
+    elm.style.background = "yellow";
+
+} // end of function when user typing input 
+
+function chgDefault(elm){
+    elm.style.background = "white";
+
+} // end of function when user leave input 
